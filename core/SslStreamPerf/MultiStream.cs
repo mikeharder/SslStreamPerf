8e0438e7fa999cec1c44d2eea0de1eea72594c0b
Mike Harder <mharder@microsoft.com>
2017-01-30 19:13:23 -0800

    Add MultiStream

---
+++ core/SslStreamPerf/MultiStream.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SslStreamPerf
+{
+    public class MultiStream : Stream
+    {
+        private readonly Stream[] _streams;
+
+        private readonly int _blockLength;
+        private long _position;
+
+        public MultiStream(IEnumerable<Stream> streams, int blockLength = 16 * 1024)
+        {
+            _streams = streams.ToArray();
+            _blockLength = blockLength;
+        }
+
+        public override bool CanRead
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override bool CanSeek
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override bool CanWrite
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override long Length
+        {
+            get
+            {
+                return _streams.Sum(s => s.Length);
+            }
+        }
+
+        public override long Position
+        {
+            get
+            {
+                return _position;
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            foreach (var stream in _streams)
+            {
+                stream.Dispose();
+            }
+        }
+
+        public override void Flush()
+        {
+            foreach (var stream in _streams)
+            {
+                stream.Flush();
+            }
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            var totalBytesRead = 0;
+            while (totalBytesRead < count)
+            {
+                var currentStream = _streams[(_position / _blockLength) % _streams.Length];
+                var remainingBytesInCurrentStream = _blockLength - (int)(_position % _blockLength);
+
+                var remainingBytes = Math.Min(count - totalBytesRead, remainingBytesInCurrentStream);
+
+                var bytesRead = currentStream.Read(buffer, offset + totalBytesRead, remainingBytes);
+
+                totalBytesRead += bytesRead;
+                _position += bytesRead;
+            }
+            return totalBytesRead;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            var bytesWritten = 0;
+            while (bytesWritten < count)
+            {
+                var currentStream = _streams[(_position / _blockLength) % _streams.Length];
+                var remainingBytesInCurrentStream = _blockLength - (int)(_position % _blockLength);
+
+                var remainingBytes = Math.Min(count - bytesWritten, remainingBytesInCurrentStream);
+
+                currentStream.Write(buffer, offset + bytesWritten, remainingBytes);
+
+                bytesWritten += remainingBytes;
+                _position += remainingBytes;
+            }
+        }
+    }
+}
--- core/SslStreamPerf/Program.cs
+++ core/SslStreamPerf/Program.cs
@@ -16,14 +16,20 @@ namespace SslStreamPerf
     {
         private class CommonOptions
         {
-            [Option('s', "sync")]
-            public bool Sync { get; set; }
-
             [Option('b', "bufferLength", Default = 1024 * 1024)]
             public int BufferLength { get; set; }
 
+            [Option('c', "connections", Default = 1)]
+            public int Connections { get; set; }
+
+            [Option('u', "multiStream")]
+            public bool MultiStream { get; set; }
+
             [Option('p', "port", Default = 8080)]
             public int Port { get; set; }
+
+            [Option('s', "sync")]
+            public bool Sync { get; set; }
         }
 
         [Verb("server")]
@@ -38,9 +44,6 @@ namespace SslStreamPerf
         {
             [Option('h', "host", Required = true)]
             public string Host { get; set; }
-
-            [Option('c', "connections", Default = 1)]
-            public int Connections { get; set; }
         }
 
         public static int Main(string[] args)
@@ -61,19 +64,37 @@ namespace SslStreamPerf
             var listener = new TcpListener(IPAddress.Any, options.Port);
             listener.Start();
 
-            Console.WriteLine($"Sync: {options.Sync}");
             Console.WriteLine($"BufferLength: {string.Format("{0:n0}", options.BufferLength)}");
             Console.WriteLine($"Megabytes: {string.Format("{0:n0}", options.Megabytes)}");
+            Console.WriteLine($"MultiStream: {options.MultiStream}");
+            Console.WriteLine($"Sync: {options.Sync}");
             Console.WriteLine();
 
             Console.WriteLine($"Listening on port {options.Port}...");
 
-            while (true)
+            if (options.MultiStream)
             {
-                var client = await listener.AcceptTcpClientAsync();
+                while (true)
+                {
+                    var clients = new TcpClient[options.Connections];
+                    for (var i = 0; i < options.Connections; i++)
+                    {
+                        clients[i] = await listener.AcceptTcpClientAsync();
+                    }
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                SendDataAsync(options, cert, client);
+                    SendDataAsync(options, cert, clients);
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                }
+            }
+            else
+            {
+                while (true)
+                {
+                    var client = await listener.AcceptTcpClientAsync();
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                    SendDataAsync(options, cert, client);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                }
             }
         }
 
@@ -105,28 +126,87 @@ namespace SslStreamPerf
             }
         }
 
+        private static async Task SendDataAsync(ServerOptions options, X509Certificate2 cert, TcpClient[] clients)
+        {
+            var data = new ZeroStream(options.Megabytes * 1024 * 1024 * options.Connections);
+
+            try
+            {
+                var streams = new Stream[clients.Length];
+                for (var i=0; i < streams.Length; i++)
+                {
+                    var stream = new SslStream(clients[i].GetStream());
+                    await stream.AuthenticateAsServerAsync(cert);
+                    streams[i] = stream;
+                }
+
+                using (var multiStream = new MultiStream(streams))
+                {
+                    Console.WriteLine();
+                    Console.WriteLine($"Sending {string.Format("{0:n0}", data.Length)} bytes...");
+
+                    var sw = Stopwatch.StartNew();
+                    if (options.Sync)
+                    {
+                        data.CopyTo(multiStream, options.BufferLength);
+                    }
+                    else
+                    {
+                        await data.CopyToAsync(multiStream, options.BufferLength);
+                    }
+                    sw.Stop();
+
+                    var mbps = ((data.Length * 8) / (1024 * 1024)) / sw.Elapsed.TotalSeconds;
+                    Console.WriteLine($"Sent {string.Format("{0:n0}", data.Length)} bytes in {Math.Round(sw.Elapsed.TotalSeconds, 3)} seconds ({mbps} Mbps)");
+                }
+            }
+            finally
+            {
+                foreach (var client in clients)
+                {
+                    client.Dispose();
+                }
+            }
+        }
+
         private static async Task<int> RunClientAsync(ClientOptions options)
         {
-            Console.WriteLine($"Sync: {options.Sync}");
             Console.WriteLine($"BufferLength: {string.Format("{0:n0}", options.BufferLength)}");
+            Console.WriteLine($"MultiStream: {options.MultiStream}");
+            Console.WriteLine($"Sync: {options.Sync}");
             Console.WriteLine();
 
             Console.WriteLine($"Connecting to {options.Host}:{options.Port}...");
 
             RemoteCertificateValidationCallback rcvc = (object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) => true;
 
-            var tasks = new Task<long>[options.Connections];
-
+            var totalBytesRead = (long)0;
             var sw = Stopwatch.StartNew();
-            for (var i = 0; i < options.Connections; i++)
+            if (options.MultiStream)
             {
-                tasks[i] = RunClientAsync(options, rcvc);
-            }
-            await Task.WhenAll(tasks);
-            sw.Stop();
+                var clients = new TcpClient[options.Connections];
+                for (var i=0; i < clients.Length; i++)
+                {
+                    var client = new TcpClient();
+                    await client.ConnectAsync(options.Host, options.Port);
+                    clients[i] = client;
+                }
 
-            var totalBytesRead = tasks.Select(t => t.Result).Sum();
-            
+                await RunClientAsync(options, rcvc, clients);
+            }
+            else
+            {
+                var tasks = new Task<long>[options.Connections];
+                for (var i = 0; i < options.Connections; i++)
+                {
+                    tasks[i] = RunClientAsync(options, rcvc);
+                }
+                await Task.WhenAll(tasks);
+                totalBytesRead = tasks.Select(t => t.Result).Sum();
+
+            }
+
+            sw.Stop();
             var mbps = ((totalBytesRead * 8) / (1024 * 1024)) / sw.Elapsed.TotalSeconds;
             Console.WriteLine($"Read {string.Format("{0:n0}", totalBytesRead)} bytes in " +
                 $"{Math.Round(sw.Elapsed.TotalSeconds, 3)} seconds ({Math.Round(mbps, 1)} Mbps)");
@@ -163,6 +243,51 @@ namespace SslStreamPerf
                     }
 
                     return totalBytesRead;
+                }
+            }
+        }
+
+        private static async Task<long> RunClientAsync(ClientOptions options, RemoteCertificateValidationCallback rcvc, TcpClient[] clients)
+        {
+            try
+            {
+                var streams = new Stream[clients.Length];
+                for (var i=0; i < streams.Length; i++)
+                {
+                    var stream = new SslStream(clients[i].GetStream(), leaveInnerStreamOpen: false, userCertificateValidationCallback: rcvc);
+                    await stream.AuthenticateAsClientAsync(options.Host);
+                    streams[i] = stream;
+                }
+
+                using (var multiStream = new MultiStream(streams))
+                {
+                    var buffer = new byte[options.BufferLength];
+                    var bytesRead = -1;
+                    var totalBytesRead = (long)0;
+
+                    if (options.Sync)
+                    {
+                        while ((bytesRead = multiStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            totalBytesRead += bytesRead;
+                        }
+                    }
+                    else
+                    {
+                        while ((bytesRead = await multiStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                        {
+                            totalBytesRead += bytesRead;
+                        }
+                    }
+
+                    return totalBytesRead;
+                }
+            }
+            finally
+            {
+                foreach (var client in clients)
+                {
+                    client.Dispose();
                 }
             }
         }
